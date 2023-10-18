09fd19757becd47cd7b2de879c90d60c2788c518
Hậu Trần <110884738+hautran-02@users.noreply.github.com>
2023-10-18 22:45:22 +0700

    add library

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      ".runtimeConfig": "./runtimeConfig.browser",
+      "~": path.resolve(__dirname, "./src"),
+    },
+  },
   plugins: [react()],
 })
