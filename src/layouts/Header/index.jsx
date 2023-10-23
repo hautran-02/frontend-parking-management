b59f1d3fa55460ab80eee828503afb9d9a86e628
Hậu Trần <110884738+hautran-02@users.noreply.github.com>
2023-10-23 22:19:24 +0700

    add logo

--- src/layouts/Header/index.jsx
+++ src/layouts/Header/index.jsx
@@ -1,7 +1,12 @@
-import { Layout } from "antd";
+import { Image, Layout, Space, theme } from "antd";
 import React from "react";
+import FULL_LOGO from "~/assets/logo/logo-text.svg";
 
 function Header({}) {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
   return (
     <Layout.Header
       style={{
@@ -11,9 +16,15 @@ function Header({}) {
         width: "100%",
         display: "flex",
         alignItems: "center",
+        background: colorBgContainer,
+        height: 64,
+        boxShadow: "0px 4px 16px 0px rgba(0, 0, 0, 0.16)",
       }}
+      className="px-4 py-2"
     >
-      <div>Header</div>
+      <Space>
+        <Image src={FULL_LOGO} preview={false} width={280}/>
+      </Space>
     </Layout.Header>
   );
 }
