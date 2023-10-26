5334ca2cf0bfac6cc8dba08ab15042047d27491c
Hậu Trần <110884738+hautran-02@users.noreply.github.com>
2023-10-26 20:37:41 +0700

    dev Gauge chart

--- src/pages/Home/components/GeneralCard.jsx
+++ src/pages/Home/components/GeneralCard.jsx
@@ -9,31 +9,69 @@ function GeneralCard({}) {
   console.log(token);
 
   const config = {
-    height: 140,
+    height: 170,
     percent: 0.75,
     range: {
       color: "l(0) 0:#B8E1FF 1:#3D76DD",
     },
     startAngle: Math.PI,
     endAngle: 2 * Math.PI,
-    indicator: null,
-    innerRadius: 0.8,
+    innerRadius: 0.84,
+    min: 0,
+    indicator: {
+      pointer: null,
+      pin: null,
+    },
+    xAxis: {
+      grid: {
+        line: {
+          style: {
+            stroke: "black",
+            lineWidth: 24,
+            lineDash: [4, 5],
+            strokeOpacity: 0.7,
+            shadowColor: "black",
+            shadowBlur: 10,
+            shadowOffsetX: 5,
+            shadowOffsetY: 5,
+            cursor: "pointer",
+          },
+        },
+      },
+    },
+    axis: {
+      label: {
+        offset: 5,
+        offsetY: 25,
+        style: { fill: "#000", fontSize: 14, color: "#000" },
+        formatter: (val) => {
+          if (val === "0") {
+            return "0";
+          } else if (val === "1") {
+            return 1;
+          }
+        },
+      },
+      subTickLine: null,
+      tickLine: null,
+    },
     statistic: {
       title: {
-        offsetY: -36,
+        offsetY: -44,
         style: {
-          fontSize: "36px",
-          color: "#4B535E",
+          fontSize: 26,
+          color: "#000",
         },
         formatter: () => "70%",
       },
       content: {
+        offsetY: -24,
         style: {
-          fontSize: "24px",
+          fontSize: "18px",
           lineHeight: "44px",
           color: "#4B535E",
         },
-        formatter: () => "加载进度",
+        formatter: () => "Xe đang đỗ",
       },
     },
     gaugeStyle: {
@@ -41,9 +79,9 @@ function GeneralCard({}) {
       lineDash: [4, 5],
       strokeOpacity: 0.7,
       shadowColor: token.boxShadowCard,
-      shadowBlur: 1,
-      shadowOffsetX: 1,
-      shadowOffsetY: 1,
+      shadowBlur: 10,
+      shadowOffsetX: 5,
+      shadowOffsetY: 5,
     },
   };
   return (
--- src/pages/Home/data.js
+++ src/pages/Home/data.js
@@ -3,18 +3,18 @@ export const DefaultPosition = [
     order: 1,
     col: 1,
     colSpan: 4,
-    rowSpan: 12,
+    rowSpan: 14,
   },
   {
     order: 2,
     col: 5,
     colSpan: 4,
-    rowSpan: 12,
+    rowSpan: 14,
   },
   {
     order: 3,
     col: 9,
     colSpan: 4,
-    rowSpan: 12,
+    rowSpan: 14,
   },
 ];
