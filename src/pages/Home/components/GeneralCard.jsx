62b76f07174c604d03e07c030e089f441184a2c2
Hậu Trần <110884738+hautran-02@users.noreply.github.com>
2023-11-04 10:07:51 +0700

    set Interval GeneralCard

--- src/pages/Home/components/GeneralCard.jsx
+++ src/pages/Home/components/GeneralCard.jsx
@@ -1,19 +1,23 @@
-import { Gauge } from "@ant-design/plots";
-import { Card, Space, Tag, Typography, theme } from "antd";
-import React from "react";
-import CardBlock from "~/components/CardBlock";
-import CustomedTag from "~/components/CustomedTag";
+import { Gauge } from '@ant-design/plots';
+import { Card, Space, Tag, Typography, theme } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { MonitorApi } from '~/api';
+import CardBlock from '~/components/CardBlock';
+import CustomedTag from '~/components/CustomedTag';
 
-function GeneralCard({ entity = "A" }) {
+function GeneralCard({ zone = 'A' }) {
   const { token } = theme.useToken();
-
-  console.log(token);
+  const [data, setData] = useState({
+    total: 1,
+    occupied: 0,
+    unoccupied: 0
+  });
 
   const config = {
     height: 170,
-    percent: 0.75,
+    percent: data.occupied / data.total,
     range: {
-      color: "l(0) 0:#B8E1FF 1:#3D76DD",
+      color: 'l(0) 0:#B8E1FF 1:#3D76DD'
     },
     startAngle: Math.PI,
     endAngle: 2 * Math.PI,
@@ -21,80 +25,102 @@ function GeneralCard({ entity = "A" }) {
     min: 0,
     indicator: {
       pointer: null,
-      pin: null,
+      pin: null
     },
     xAxis: {
       grid: {
         line: {
           style: {
-            stroke: "black",
+            stroke: 'black',
             lineWidth: 24,
             lineDash: [4, 5],
             strokeOpacity: 0.7,
-            shadowColor: "black",
+            shadowColor: 'black',
             shadowBlur: 10,
             shadowOffsetX: 5,
             shadowOffsetY: 5,
-            cursor: "pointer",
-          },
-        },
-      },
+            cursor: 'pointer'
+          }
+        }
+      }
     },
     axis: {
       label: {
         offset: 5,
         offsetY: 25,
-        style: { fill: "#000", fontSize: 14, color: "#000" },
+        style: { fill: '#000', fontSize: 14, color: '#000' },
         formatter: (val) => {
-          if (val === "0") {
-            return "0";
-          } else if (val === "1") {
-            return 1;
+          if (val === '0') {
+            return '0';
+          } else if (val === '1') {
+            return val * data.total;
           }
-        },
+        }
       },
       subTickLine: null,
-      tickLine: null,
+      tickLine: null
     },
     statistic: {
       title: {
         offsetY: -44,
         style: {
           fontSize: 26,
-          color: "#000",
+          color: '#000'
         },
-        formatter: () => "70%",
+        formatter: (e) => data.occupied.toString()
       },
       content: {
         offsetY: -24,
         style: {
-          fontSize: "18px",
-          lineHeight: "44px",
-          color: "#4B535E",
+          fontSize: '18px',
+          lineHeight: '44px',
+          color: '#4B535E'
         },
-        formatter: () => "Xe đang đỗ",
-      },
+        formatter: () => 'Xe đang đỗ'
+      }
     },
     gaugeStyle: {
-      lineCap: "round",
+      lineCap: 'round',
       lineDash: [4, 5],
       strokeOpacity: 0.7,
       shadowColor: token.boxShadowCard,
       shadowBlur: 10,
       shadowOffsetX: 5,
-      shadowOffsetY: 5,
-    },
+      shadowOffsetY: 5
+    }
   };
+
+  const callApi = async () => {
+    try {
+      const api = await MonitorApi.getStatusByZone(zone);
+      if (api) {
+        setData({ ...api });
+      }
+    } catch {}
+  };
+
+  useEffect(() => {
+    callApi();
+    const interval = setInterval(
+      () => {
+        callApi();
+      },
+      import.meta.env.VITE_INTERVAL_TIME
+    );
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
   return (
     <Card
       title={
-        <CustomedTag bordered={false} entity={entity} entityType={"zone"}>
+        <CustomedTag bordered={false} entity={zone} entityType={'zone'}>
           Khu A
         </CustomedTag>
       }
-      className="card-main"
-    >
+      className="card-main">
       <CardBlock>
         <div className="py-1">
           <Gauge {...config} />
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,16 +1,16 @@
-import { TileLayout } from "@progress/kendo-react-layout";
-import { Col, Layout, Row } from "antd";
-import React, { useEffect, useRef, useState } from "react";
-import { PageHeader } from "~/components";
-import { Content, Footer, Header } from "~/layouts";
-import { DefaultPosition } from "./data";
-import { EventBlock, GeneralCard, NumberStatisChart } from "./components";
-import RevenueChart from "./components/RevenueChart";
+import { TileLayout } from '@progress/kendo-react-layout';
+import { Col, Layout, Row } from 'antd';
+import React, { useEffect, useRef, useState } from 'react';
+import { PageHeader } from '~/components';
+import { Content, Footer, Header } from '~/layouts';
+import { DefaultPosition } from './data';
+import { EventBlock, GeneralCard, NumberStatisChart } from './components';
+import RevenueChart from './components/RevenueChart';
 
 const dynamicBlock = {
   resizable: false,
   reorderable: false,
-  className: "hide-header",
+  className: 'hide-header'
 };
 
 function Home({}) {
@@ -20,25 +20,25 @@ function Home({}) {
   const hanldeLayout = () => {
     const rs = [
       {
-        body: <GeneralCard />,
-        ...dynamicBlock,
+        body: <GeneralCard zone="A" />,
+        ...dynamicBlock
       },
       {
-        body: <GeneralCard />,
-        ...dynamicBlock,
+        body: <GeneralCard zone="B" />,
+        ...dynamicBlock
       },
       {
-        body: <GeneralCard />,
-        ...dynamicBlock,
+        body: <GeneralCard zone="C" />,
+        ...dynamicBlock
       },
       {
         body: <NumberStatisChart />,
-        ...dynamicBlock,
+        ...dynamicBlock
       },
       {
         body: <RevenueChart />,
-        ...dynamicBlock,
-      },
+        ...dynamicBlock
+      }
     ];
 
     setLayoutItems(rs);
@@ -50,7 +50,7 @@ function Home({}) {
 
   return (
     <Layout className="px-4">
-      <Header className="border-1" title={"Dashboard"} />
+      <Header className="border-1" title={'Dashboard'} />
       <Content className="w-100 py-3">
         <Row id="dashboard-block" gutter={16}>
           <Col className="gutter-row" span={16}>
@@ -59,17 +59,17 @@ function Home({}) {
               columns={12}
               rowHeight={4}
               gap={{ rows: 16, columns: 16 }}
-              id={"dashboardLayout"}
+              id={'dashboardLayout'}
               positions={DefaultPosition}
               items={layoutItems}
               style={{
                 padding: 0,
-                width: "100%",
+                width: '100%'
               }}
             />
           </Col>
           <Col className="gutter-row" span={8}>
-            <EventBlock/>
+            <EventBlock />
           </Col>
         </Row>
       </Content>
