f0af3708a507440eac2b91bfc1e6695af8b6e105
Hậu Trần <110884738+hautran-02@users.noreply.github.com>
2023-12-05 23:42:09 +0700

    change component path

--- src/views/pages/Main/index.jsx
+++ src/views/pages/Main/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Layout, theme } from 'antd';
 import { Content, Footer, Header, Sider } from '~/views/layouts';
 import { Navigate, Route, Routes } from 'react-router-dom';
@@ -7,11 +7,30 @@ import Map from './Map';
 import Driver from './Driver';
 import { publicRoutes } from '~/routes';
 import { users } from './data';
+import AppContext from '~/context';
 
 function Main({}) {
   const { token } = theme.useToken();
+  const { state, actions } = useContext(AppContext);
+  const { auth } = state;
+
   return (
     <Layout className="vh-100">
+      {/* <Modal
+        title={formAction.title}
+        open={openForm}
+        onCancel={() => {
+          setOpenForm(false);
+        }}
+        destroyOnClose={true}
+        classNames={{ footer: 'd-none' }}>
+        <EmployeeForm
+          formAction={formAction}
+          isOpen={openForm}
+          onClose={hanldeCloseForm}
+          noChangeAccount
+        />
+      </Modal> */}
       <Sider style={{ background: token.colorBgContainer }} />
       <Routes>
         {publicRoutes.map((route, ix) => (
