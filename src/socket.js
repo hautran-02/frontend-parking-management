ae94330c3082b03fe73db3bd6bc2136c05993d21
Hậu Trần <110884738+hautran-02@users.noreply.github.com>
2023-12-16 23:18:32 +0700

    update socket

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-const socket = io('https://your-backend-domain.com', {
+const socket = io('https://parking-api-h07g.onrender.com/', {
   withCredentials: true, // include credentials when making cross-origin requests
   transports: ['websocket'] // use only WebSocket transport (optional)
 });
