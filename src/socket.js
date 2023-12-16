701abb0a93c8c26b1c99f4692f4fa3e93e9f1c11
Hậu Trần <110884738+hautran-02@users.noreply.github.com>
2023-12-16 21:55:34 +0700

    socket update

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
 
-const socket = io.connect('https://parking-api-h07g.onrender.com/');
+const socket = io.connect('https://parking-api-h07g.onrender.com:3000');
 
 export default socket;
